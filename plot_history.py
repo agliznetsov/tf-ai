e7b05ff7eb99baeaaa62daa231a38d19437a6bfd
Andrey Gliznetsov <a.gliznetsov@evs.com>
2018-12-27 17:10:05 +0100

    ttt

--- plot_history.py
+++ plot_history.py
@@ -2,14 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_history(histories):
-    plt.figure(figsize=(16, 10))
+def plot_history(histories, figures):
+    plt.figure(1, figsize=(4, 6))
+    index = 0
 
     for name, history in histories:
-        for key in history.history:
-            plt.plot(history.epoch, history.history[key], label=key.title())
+        for keys in figures:
+            index = index + 1
+            plt.subplot(2, 1, index)
+            plt.xlabel('Epochs')
+            plt.ylabel(keys[0])
+            for key in keys:
+                plt.plot(history.epoch, history.history[key], label=key.title())
+            plt.legend()
 
-    plt.xlabel('Epochs')
-    # plt.ylabel(key.replace('_', ' ').title())
-    plt.legend()
     plt.show()
--- ttt.py
+++ ttt.py
@@ -2,25 +2,28 @@ import numpy as np
 import tensorflow as tf
 from plot_history import *
 
-data = np.genfromtxt('./data/t.csv', delimiter=',')
+data = np.genfromtxt('./data/ttt.csv', delimiter=',')
 np.random.shuffle(data)
 m = data.shape[0]
-m_train = int(m * 0.5)
 data = np.split(data, [9], axis=1)
-X = np.split(data[0], [m_train])
-Y = np.split(data[1], [m_train])
+X = data[0]
+# X = X * -1 + 1
+Y = data[1]
+print(X)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(9, kernel_regularizer=tf.keras.regularizers.l2(0.1), activation=tf.nn.relu),
-    # tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(9, kernel_regularizer=tf.keras.regularizers.l2(0.1), activation=tf.nn.softmax)
+    tf.keras.layers.Dense(400, kernel_regularizer=tf.keras.regularizers.l2(0.01), activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(200, kernel_regularizer=tf.keras.regularizers.l2(0.01), activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(9, activation=tf.nn.softmax)
 ])
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-history = model.fit(X[0], Y[0], epochs=500, verbose=0)
-res = model.evaluate(X[1], Y[1], verbose=0)
-print('Test evaluation: ' + str(res))
+history = model.fit(X, Y, validation_split=0.2, epochs=200, verbose=1)
+# res = model.evaluate(X[1], Y[1], verbose=0)
+# print('Test accuracy: ' + str(res[1]))
 
-plot_history([('baseline', history)])
+plot_history([('baseline', history)], [['loss', 'val_loss'], ['acc', 'val_acc']])

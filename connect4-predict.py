4bc8ae5fe7498054275637224ba2ba479c001081
Andrey Gliznetsov <a.gliznetsov@evs.com>
2019-01-10 15:22:08 +0100

    connect4 convolution

--- connect4-predict.py
+++ connect4-predict.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+
 from plot_history import *
 
 labels = {
@@ -19,7 +20,8 @@ converters = {}
 for i in range(0, 43):
     converters[i] = parse
 
-data = np.genfromtxt('./data/connect4-predict.csv', delimiter=',', dtype=float, converters=converters, encoding='latin1') # converters=converters,
+data = np.genfromtxt('./data/connect4-predict.csv', delimiter=',', dtype=float, converters=converters,
+                     encoding='latin1')  # converters=converters,
 
 # np.save('./data/connect4-predict.npy', data)
 # data = np.load('./data/connect4-predict.npy')
@@ -36,19 +38,28 @@ Y = data[1]
 # tmp = np.flip(X[0].reshape((7, 6)).T)
 # print(tmp)
 
+# model = tf.keras.models.Sequential([
+#     tf.keras.layers.Dense(50, activation=tf.nn.relu, kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+#     # tf.keras.layers.Dropout(0.3),
+#     tf.keras.layers.Dense(3, activation=tf.nn.softmax)
+# ])
+
+X = X.reshape(X.shape[0], 7, 6, 1).astype('float32')
+
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(150, activation=tf.nn.relu, kernel_regularizer=tf.keras.regularizers.l2(0.001)),
-    # tf.keras.layers.Dropout(0.3),
-    tf.keras.layers.Dense(150, activation=tf.nn.relu, kernel_regularizer=tf.keras.regularizers.l2(0.001)),
-    # tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Conv2D(16, kernel_size=3, padding="same", activation='relu'),
+    tf.keras.layers.Conv2D(16, kernel_size=3, padding="same", activation='relu'),
+    tf.keras.layers.Conv2D(16, kernel_size=3, padding="same", activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(50, activation=tf.nn.relu, kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+    tf.keras.layers.Dropout(0.1),
     tf.keras.layers.Dense(3, activation=tf.nn.softmax)
 ])
+
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 history = model.fit(X, Y, validation_split=0.2, epochs=15, verbose=1)
-# res = model.evaluate(X[1], Y[1], verbose=0)
-# print('Test accuracy: ' + str(res[1]))
-
+model.save_weights('./model/connect4-conv.h5')
 plot_history([('baseline', history)], [['loss', 'val_loss'], ['acc', 'val_acc']])
